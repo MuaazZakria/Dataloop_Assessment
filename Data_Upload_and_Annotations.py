1eb78220c9e47a45a6f99705e0e2f932aa88c9eb
Muaaz Zakria <60637386+MuaazZakria@users.noreply.github.com>
2023-09-27 12:26:55 +0500

    Update Data_Upload_and_Annotations.py
    1.  The script tries getting a project/dataset, and only if it does not exist, creates a new one
    
    2. Use add_labels instead of using add_label 5 times 
    
    3.  Added metadata during image upload
    
    4.  Automated the upload of annotations

--- Data_Upload_and_Annotations.py
+++ Data_Upload_and_Annotations.py
@@ -13,99 +13,68 @@ import json
 if dl.token_expired():
     dl.login()
     
-######Creating and getting the project######
+######Creating / getting the project & dataset######
 
-project = dl.projects.create(project_name='My-Project')    
+try:
+    project = dl.projects.get(project_name='My-Project')
+    dataset = project.datasets.get(dataset_name='My-Dataset')
+except:
+    project = dl.projects.create(project_name='My-Project')
+    project = dl.projects.get(project_name='My-Project')
+    dataset = project.datasets.create(dataset_name='My-Dataset')
+    dataset = project.datasets.get(dataset_name='My-Dataset')
 
-project = dl.projects.get(project_name='My-Project')
-
-######Creating and getting the dataset######
-
-dataset = project.datasets.create(dataset_name='My-Dataset')
-
-dataset = project.datasets.get(dataset_name='My-Dataset')
-
-######Uploading the images######
+######Uploading the images by adding UTM metadata######
 
 local_path = [r"C:\Users\DELL\ML Solution Engineering Home Assignment\images"]
-
-dataset = project.datasets.get(dataset_name='My-Dataset')
-dataset.items.upload(local_path=local_path)
-
-######Adding the labels######
-
-dataset.add_label(label_name='1')
-dataset.add_label(label_name='2')
-dataset.add_label(label_name='3')
-dataset.add_label(label_name='top')
-dataset.add_label(label_name='bottom')
-
-######Adding UTM metadata######
 
 # Get the current Unix timestamp
 timestamp = int(datetime.utcnow().timestamp())
 
-filters = dl.Filters()
+item_metadata = {'user': {'collected': timestamp}}
 
-dataset.items.update(filters = filters, update_values={'user': {'collected': timestamp}})
+dataset.items.upload(local_path=local_path,
+                     item_metadata=item_metadata)
+
+######Adding the labels######
+
+dataset.add_labels(label_list=['1', '2', '3', 'top', 'bottom'])
 
 
 ######Uploading the annotations######
 
 with open(r'C:\\Users\\DELL\\ML Solution Engineering Home Assignment\ML Solution Engineering Home Assignment.json', 'r') as f:
     data = json.load(f)
+    
+pages = dataset.items.list()
+for item in pages.all():
+    builder = item.annotations.builder()
 
+    for annotation_data in data[item.name]:
+        label = annotation_data['label']
+        confidence = annotation_data['confidence']
 
-data_1 = data['1.jpg']
-data_2 = data['2.jpg']
-data_3 = data['3.jpg']
+        if 'point' in annotation_data:
+            point_data = annotation_data['point']
+            x = point_data['x']
+            y = point_data['y']
+            builder.add(annotation_definition=dl.Point(x=x, y=y, label=label),
+                            model_info={'name': 'confidence', 'confidence': confidence})
 
-###Image 1###
-item_1 = dataset.items.get(item_id='6507f51a3d477e1eaf86300f')
-builder = item_1.annotations.builder()
-builder.add(annotation_definition=dl.Point(x=data_1[1]['point']['x'], y=data_1[1]['point']['y'], 
-                                          label=data_1[1]['label']),
-           model_info={'name': 'confidence', 'confidence': data_1[1]['confidence']})
+        elif 'box' in annotation_data:
+            box_data = annotation_data['box']
+            x1, y1 = box_data[0]['x'], box_data[0]['y']
+            x2, y2 = box_data[1]['x'], box_data[1]['y']
+            builder.add(annotation_definition=dl.Box(top=y1, left=x1, bottom=y2, right=x2, label=label),
+                            model_info={'name': 'confidence', 'confidence': confidence})
 
-builder.add(annotation_definition=dl.Box(top=data_1[0]['box'][0]['y'],
-                                         left=data_1[0]['box'][0]['x'],
-                                         bottom=data_1[0]['box'][1]['x'],
-                                         right=data_1[0]['box'][1]['y'],
-                                         label=data_1[0]['label']),
-           model_info={'name': 'confidence', 'confidence': data_1[0]['confidence']})
+        elif 'polygon' in annotation_data:
+            polygon_coordinates = annotation_data['polygon']
 
-builder.add(annotation_definition=dl.Point(x=data_1[2]['point']['x'], y=data_1[2]['point']['y'], 
-                                          label=data_1[2]['label']),
-           model_info={'name': 'confidence', 'confidence': data_1[2]['confidence']})
+            builder.add(annotation_definition=dl.Polygon(geo=polygon_coordinates, label=label),
+                            model_info={'name': 'confidence', 'confidence': confidence})
 
-polygon_coordinates = list(map(lambda point: [point['x'], point['y']], data_1[3]['polygon'][0]))
-
-builder.add(annotation_definition=dl.Polygon(geo=polygon_coordinates,
-                                             label=data_1[3]['label']),
-           model_info={'name': 'confidence', 'confidence': data_1[3]['confidence']})
-item_1.annotations.upload(builder)
-
-###Image 2###
-item_2 = dataset.items.get(item_id='6507f51a06d51ddc067628ec')
-builder = item_2.annotations.builder()
-builder.add(annotation_definition=dl.Box(top=data_2[0]['box'][0]['y'],
-                                         left=data_2[0]['box'][0]['x'],
-                                         bottom=data_2[0]['box'][1]['x'],
-                                         right=data_2[0]['box'][1]['y'],
-                                         label=data_2[0]['label']),
-           model_info={'name': 'confidence', 'confidence': data_2[0]['confidence']})
-
-item_2.annotations.upload(builder)
-
-###Image 3###
-item_3 = dataset.items.get(item_id='6507f51ac040ad3e6fed90e5')
-builder = item_3.annotations.builder()
-builder.add(annotation_definition=dl.Point(x=data_3[0]['point']['x'], y=data_3[0]['point']['y'], 
-                                          label=data_3[0]['label']),
-           model_info={'name': 'confidence', 'confidence': data_3[0]['confidence']})
-
-builder.add(annotation_definition=dl.Point(x=data_3[1]['point']['x'], y=data_3[1]['point']['y'], 
-                                          label=data_3[1]['label']),
-           model_info={'name': 'confidence', 'confidence': data_3[1]['confidence']})
-
-item_3.annotations.upload(builder)
+        try:
+            item.annotations.upload(builder)
+        except:
+            pass
